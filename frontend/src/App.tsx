454390dca03196735e7a9653b7773ce8f18e6d78
nnitheesh863-maker <nnitheesh863@gmail.com>
2026-09-05 16:00:26 +0530

    app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,12 @@ import { AppRoutes } from './routes/AppRoutes';
 
 export function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
